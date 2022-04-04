ad94ea359529ca556b8465f2ee2f6dc0bf6c52a3
Joel Deon Dsouza <oka.welt.dev@gmail.com>
2022-04-04 14:57:53 +0200

    Dependencies added to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import { useState } from "react";
+import { useQuery } from "react-query";
+
+import Drawer from "@material-ui/core/Drawer";
+import LinearProgress from "@material-ui/core/LinearProgress";
+import Grid from "@material-ui/core/Grid";
+import Badge from "@material-ui/core/Badge";
+import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
+
 const App = () => {
   return (
     <div className="App">
