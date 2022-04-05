ec8a0aa233b4adab67ba1ce7014757bf23ff3914
Joel Deon Dsouza <oka.welt.dev@gmail.com>
2022-04-05 13:30:40 +0200

    CartIcon Added to the app

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import Grid from "@material-ui/core/Grid";
 import Badge from "@material-ui/core/Badge";
 import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
 
-import { Wrapper } from "./App.styles";
+import { Wrapper, StyledButton } from "./App.styles";
 import Item from "./Components/Item/item";
 
 export type CartItem = {
@@ -24,13 +24,15 @@ const getProducts = async (): Promise<CartItem[]> =>
   await (await fetch("https://fakestoreapi.com/products")).json();
 
 const App = () => {
+  const [cartOpen, setCartOpen] = useState(false);
+  const [cartItems, setCartItems] = useState([] as CartItem[]);
   const { data, isLoading, error } = useQuery<CartItem[]>(
     "products",
     getProducts
   );
   console.log(data);
 
-  const getTotalItems = () => null;
+  const getTotalItems = (items: CartItem[]) => null;
 
   const handleAddToCart = (clickedItem: CartItem) => null;
 
@@ -41,6 +43,16 @@ const App = () => {
 
   return (
     <Wrapper>
+      <Drawer
+        anchor="right"
+        open={cartOpen}
+        onClose={() => setCartOpen(false)}
+      ></Drawer>
+      <StyledButton onClick={() => setCartOpen(true)}>
+        <Badge badgeContent={getTotalItems(cartItems)} color="error">
+          <AddShoppingCartIcon />
+        </Badge>
+      </StyledButton>
       <Grid container spacing={3}>
         {data?.map((item) => (
           <Grid item key={item.id} xs={12} sm={4}>
